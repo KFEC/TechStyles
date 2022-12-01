aec75c3db82c4f8e78c69dfa291b8c669f97bcf4
akbarimo <mo.akbari15@gmail.com>
2022-11-30 23:38:21 -0800

    Implemented server side starter code

---
+++ server/index.js
@@ -0,0 +1,12 @@
+require('dotenv').config();
+const express = require('express');
+const path = require('path');
+const morgan = require('morgan');
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.use(express.static(path.join(__dirname, '/../client/dist')));
+app.use(morgan('dev'));
+
+app.listen(PORT, console.log(`Now listening on ${PORT}`));
