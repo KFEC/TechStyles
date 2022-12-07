a30b59ff26336cecd4a6c28ec97cfa48e6ba04ed
Robert Hu <robert.y.hu.97@gmail.com>
2022-12-07 12:51:26 -0800

    Review List rendering

--- client/src/components/reviews/components/ReviewList.jsx
+++ client/src/components/reviews/components/ReviewList.jsx
@@ -2,12 +2,19 @@ import React from 'react';
 import { Div } from '../../../lib/styledComponents';
 import ReviewListEntry from './ReviewListEntry.jsx';
 
-const ReviewList = () => {
+
+const ReviewList = ({ reviews }) => {
+
 
   return (
     <Div>
       Review List
-      <ReviewListEntry />
+      {reviews.map(review => (
+        <ReviewListEntry
+          key={review.review_id}
+          review={review}
+        />
+      ))}
     </Div>
   );
 };
