5893e7ea9a5b3eb823aa6cd50bff329518aa4125
akbarimo <mo.akbari15@gmail.com>
2022-12-07 22:39:26 -0800

    Created unit test

--- client/src/components/reviews/components/ratingbreakdown/RatingBreakdown.jsx
+++ client/src/components/reviews/components/ratingbreakdown/RatingBreakdown.jsx
@@ -35,7 +35,7 @@ const RatingBreakdown = () => {
   }, []);
 
   return (
-    <div>
+    <div data-testid="rating-breakdown">
       {ratings
       && <RBHeading ratings={{ totalRatings, ratings, recommendedCt }} />}
       {ratings
