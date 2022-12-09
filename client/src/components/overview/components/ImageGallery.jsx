be690c721939119c8a5feb3881f39a6dc026d6cb
Madeline King <madeline.king330@gmail.com>
2022-12-08 22:22:34 -0800

    completed image carousel with full functionality and fixed left/right arrow bugs

--- client/src/components/overview/components/ImageGallery.jsx
+++ client/src/components/overview/components/ImageGallery.jsx
@@ -8,6 +8,7 @@ import {
 const ImageGallery = ({ gallery }) => {
   const [main, setMain] = useState('');
   const [currentIndex, setCurrentIndex] = useState(0);
+  const [carouselIndex, setCarouselIndex] = useState([0, 6]);
   let newIndex = 0;
 
   useEffect(() => {
@@ -20,22 +21,25 @@ const ImageGallery = ({ gallery }) => {
     console.log(event.target.name);
     const changeMain = gallery[event.target.name];
     setMain(changeMain);
-    setCurrentIndex(event.target.name);
+    // console.log('check onClick', typeof event.target.name);
+    // FOUND THE BUG
+    setCurrentIndex(Number(event.target.name));
   };
 
   const handleLeftClick = () => {
     newIndex = currentIndex === 0 ? gallery.length - 1 : currentIndex - 1;
-    setMain(gallery[newIndex]); // bugs, renderMain() triggered before image is set
+    setMain(gallery[newIndex]);
     setCurrentIndex(newIndex);
   };
 
   const handleRightClick = () => {
     newIndex = currentIndex === gallery.length - 1 ? 0 : currentIndex + 1;
-    setMain(gallery[newIndex]); // bugs, renderMain() triggered before image is set
+    setMain(gallery[newIndex]);
     setCurrentIndex(newIndex);
   };
 
   const renderMain = () => {
+    console.log('fix render main bug in image gallery');
     return (
       <div id="main-img-container-arrows">
         <IoIosArrowBack onClick={() => { handleLeftClick(); }} style={{ color: 'green', zIndex: 5, fontSize: '2.5em' }} />
@@ -49,12 +53,25 @@ const ImageGallery = ({ gallery }) => {
     );
   };
 
-  const renderGallery = () => {
-    return (
-      <div id="thumbnail-render">
 
-        {gallery.map((image, index) => {
-          if (index <= 6) {
+  const handleUpClick = () => {
+    const newCarouselIndex = [carouselIndex[0] - 1, carouselIndex[1] - 1];
+    setCarouselIndex(newCarouselIndex);
+  };
+
+  const handleDownClick = () => {
+    const newCarouselIndex = [carouselIndex[0] + 1, carouselIndex[1] + 1];
+    setCarouselIndex(newCarouselIndex);
+  };
+
+  // if current index === carouselIndex[0]
+  // then highlight the thumbnail image with that same index
+
+  const renderGallery = () => {
+    if (gallery.length <= 7) {
+      return (
+        <div id="thumbnail-render">
+          {gallery.map((image, index) => {
             return (
               <img
                 id="thumbnail-img"
@@ -70,14 +87,62 @@ const ImageGallery = ({ gallery }) => {
                 }}
               />
             );
-          }
-          return (
-            <div key={Math.random(index * 54) * 10} /> // fix this
-          );
-        })}
+          })}
+        </div>
+      );
+    } if (gallery.length > 7) {
+      return (
+        <div id="thumbnail-render">
+          {carouselIndex[0] !== 0 ? <IoIosArrowUp onClick={() => { handleUpClick(); }} style={{ color: 'black', fontSize: '3.5em' }} /> : null}
 
-      </div>
-    );
+          {gallery.map((image, index) => {
+
+            if (index === Number(currentIndex)) {
+              return (
+                <img
+                  id="thumbnail-img"
+                  name={index}
+                  onClick={(event) => { handleClick(event); }}
+                  src={`${image}`}
+                  alt=""
+                  key={Math.random(index * 54) * 10}
+                  style={{
+                    width: '100px',
+                    height: '100px',
+                    objectFit: 'cover',
+                    border: '3px solid green',
+                  }}
+                />
+              );
+            }
+
+
+            if (index >= carouselIndex[0] && index <= carouselIndex[1]
+               && index !== Number(currentIndex)) {
+              // console.log('index ', typeof index, 'currentindex ',
+              // typeof currentIndex, typeof index === typeof currentIndex);
+              return (
+                <img
+                  id="thumbnail-img"
+                  name={index}
+                  onClick={(event) => { handleClick(event); }}
+                  src={`${image}`}
+                  alt=""
+                  key={Math.random(index * 54) * 10}
+                  style={{
+                    width: '100px',
+                    height: '100px',
+                    objectFit: 'cover',
+                  }}
+                />
+              );
+            }
+          })}
+
+          {carouselIndex[1] >= 6 && carouselIndex[1] !== gallery.length - 1 ? <IoIosArrowDown onClick={() => { handleDownClick(); }} style={{ color: 'black', fontSize: '3.5em' }} /> : null}
+        </div>
+      );
+    }
   };
 
   return (
