ccf9089e8f3f14b8d19ebb5a1436071c1edbe0d7
akbarimo <mo.akbari15@gmail.com>
2022-12-09 13:51:19 -0800

    Changed naming convention

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import store from './app/store';
+import store from './store/store';
 import App from './components/App.jsx';
 import Checkout from './pages/Checkout.jsx';
 import Contact from './pages/Contact.jsx';
