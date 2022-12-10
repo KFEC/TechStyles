eb8dbbf013632df0d7a8c7605d2d410897ff8c71
Robert Hu <robert.y.hu.97@gmail.com>
2022-12-09 18:14:32 -0800

    Initial redux steps

--- client/src/components/qa/index.jsx
+++ client/src/components/qa/index.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { useSelector } from 'react-redux';
 import { Div } from '../../lib/styledComponents';
 import { getData } from '../../lib/index.js';
 
@@ -7,26 +8,31 @@ import QuestionList from './components/QuestionList.jsx';
 
 const QA = () => {
 
-  const [questions, setQuestions] = useState([]);
-  const [filter, setFilter] = useState(questions);
+  const [filter, setFilter] = useState([]);
   const [update, setUpdate] = useState(false);
 
+  const { productId, productInfo, productQuestions } = useSelector(state => state.product);
+
   useEffect(() => {
-    getData('/qa/questions', {
-      product_id: 40344,
-      count: 100,
-    })
-      .then((result) => {
-        setQuestions(result.data.results);
-        setFilter(result.data.results);
-      });
-  }, [update]);
+    setFilter(productQuestions);
+  }, [productId]);
+
+  // useEffect(() => {
+  //   getData('/qa/questions', {
+  //     product_id: 40344,
+  //     count: 100,
+  //   })
+  //     .then((result) => {
+  //       setQuestions(result.data.results);
+  //       setFilter(result.data.results);
+  //     });
+  // }, [update]);
 
   const search = (query) => {
-    if (query.length < 3) setFilter(questions);
+    if (query.length < 3) setFilter(productQuestions);
     else {
       const filtered = [];
-      questions.forEach((question) => {
+      productQuestions.forEach((question) => {
         if (question.question_body.toLowerCase().includes(query.toLowerCase())) {
           filtered.push(question);
         }
