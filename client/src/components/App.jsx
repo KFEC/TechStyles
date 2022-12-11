9617ffb4bc865aabf3a5013a21705004098ee300
Madeline King <madeline.king330@gmail.com>
2022-12-11 00:07:12 -0800

    design and add company logo

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -90,7 +90,7 @@ const App = () => {
 
   return (
     <AppDiv data-testid="app-1">
-      <Wrapper><h1>TechStyles</h1></Wrapper>
+      <Wrapper><img id="app-logo" src="./fec-logo.png" alt="TechStyles" /></Wrapper>
       <Overview />
       <RelatedProducts />
       <QA />
