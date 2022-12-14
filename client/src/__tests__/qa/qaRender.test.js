34b869a03e174f14b1998944777ec11bfebfacb4
Robert Hu <robert.y.hu.97@gmail.com>
2022-12-13 18:57:05 -0800

    Remove unnecessary lines of code and uninstalled redux-mock-store

--- client/src/__tests__/qa/qaRender.test.js
+++ client/src/__tests__/qa/qaRender.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Provider } from 'react-redux'
 import { render, screen } from '@testing-library/react';
+
 import QA from '../../components/qa/index.jsx';
 import QuestionEntry from '../../components/qa/components/QuestionEntry.jsx';
 import QuestionList from '../../components/qa/components/QuestionList.jsx';
@@ -8,44 +9,17 @@ import QuestionSearch from '../../components/qa/components/QuestionSearch.jsx';
 import AnswerEntry from '../../components/qa/components/AnswerEntry.jsx';
 import NewAnswer from '../../components/qa/components/NewAnswer.jsx';
 import NewQuestion from '../../components/qa/components/NewQuestion.jsx';
-import configureStore from 'redux-mock-store'
-import productReducer from '../../reducers/productSlice';
-import productPageReducer from '../../reducers/productPageSlice';
 
 import store from '../../store/store'
 
 describe('QA Render Breakdown', () => {
-  // const initialState = {
-  //   productPage: {
-  //     isDarkMode: true,
-  //   },
-  //   product: {
-  //     productId: '40348',
-  //     productReviews: {
-  //       allReviews: [],
-  //       stars: 0,
-  //       recommended: 0,
-  //       totalRatings: 0,
-  //       totalReviews: 0,
-  //     },
-  //     productInfo: {},
-  //     productMeta: {},
-  //     productStyles: {},
-  //     productQuestions: [],
-  //     relatedProducts: [],
-  //   },
-  // }
-  const mockStore = configureStore();
-  // let store,wrapper;
 
   test('Render QA component', () => {
-    // store = mockStore(initialState)
     render(<Provider store={store}><QA /></Provider>);
     expect(screen.getByTestId('test QA')).toBeInTheDocument();
   })
 
   test('Render QuestionEntry component', () => {
-    // store = mockStore(initialState)
     const question = {
       "question_id": 37,
       "question_body": "Why is this product cheaper here than other sites?",
@@ -70,19 +44,16 @@ describe('QA Render Breakdown', () => {
   })
 
   test('Render QuestionList component', () => {
-    // store = mockStore(initialState)
     render(<Provider store={store}><QuestionList questions={[]}/></Provider>);
     expect(screen.getByTestId('test QuestionList')).toBeInTheDocument();
   });
 
   test('Render QuestionSearch component', () => {
-    // store = mockStore(initialState)
     render(<Provider store={store}><QuestionSearch /></Provider>);
     expect(screen.getByTestId('test QuestionSearch')).toBeInTheDocument();
   })
 
   test('Render AnswerEntry component', () => {
-    // store = mockStore(initialState)
     const answer = {
       "answer_id": 8,
       "body": "What a great question!",
@@ -96,13 +67,11 @@ describe('QA Render Breakdown', () => {
   })
 
   test('Render NewAnswer component', () => {
-    // store = mockStore(initialState)
     render(<Provider store={store}><NewAnswer /></Provider>);
     expect(screen.getByTestId('test NewAnswer')).toBeInTheDocument();
   })
 
   test('Render NewQuestion component', () => {
-    // store = mockStore(initialState)
     render(<Provider store={store}><NewQuestion /></Provider>);
     expect(screen.getByTestId('test NewQuestion')).toBeInTheDocument();
   })
